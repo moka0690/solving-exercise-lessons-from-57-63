f0eeca3f1c5743626bf1a3c9431f57a5a0437686
moka0690 <madradz39@gmail.com>
2024-04-29 19:40:23 +0100

    Exercise solution  2

--- main.js
+++ main.js
@@ -11,3 +11,26 @@ function sayHello(theName, theGender) {
   sayHello("Osama", "Male"); // "Hello Mr Osama"
   sayHello("Eman", "Female"); // "Hello Miss Eman"
   sayHello("Sameh"); // "Hello Sameh"
+
+
+
+
+  function calculate(firstNum, secondNum, operation="add") {
+    if(secondNum === undefined) {
+      console.log("Second Number Not Found");
+    }
+    else if(operation === "add") {
+      console.log(firstNum + secondNum);
+    }if(operation === "subtract") {
+      console.log(firstNum - secondNum);
+    }if(operation === "multiply") {
+      console.log(firstNum * secondNum);
+    }
+  }
+  
+  // Needed Output
+  calculate(20); // Second Number Not Found
+  calculate(20, 30); // 50
+  calculate(20, 30, 'add'); // 50
+  calculate(20, 30, 'subtract'); // -10
+  calculate(20, 30, 'multiply'); // 600
